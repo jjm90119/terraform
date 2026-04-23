5b8acb64bc774a17ef639725572c3eabdeec8a3e
Joshua Shin <joshua.shin32@gmail.com>
2026-04-23 14:10:34 -0700

    configured instance settings

--- main.tf
+++ main.tf
@@ -70,3 +70,20 @@ resource "aws_key_pair" "mtv_auth" {
   key_name   = "mtvkey"
   public_key = file("~/.ssh/mtvkey.pub")
 }
+
+resource "aws_instance" "dev_node" {
+    instance_type = "t2.micro"
+    ami = data.aws_ami.server_ami.id
+
+    tags = {
+        Name = "dev-node"
+    }
+
+    key_name = aws_key_pair.mtv_auth.id
+    vpc_security_group_ids = [aws_security_group.mtv_sg.id]
+    subnet_id = aws_subnet.mtv_public_subnet.id
+
+    root_block_device {
+        volume_size = 10
+    }
+}

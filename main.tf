258384a06b0132bb98c6b2e1f58e2d8fd9b5bd6c
Joshua Shin <joshua.shin32@gmail.com>
2026-04-27 08:13:33 -0700

    added provisioner and linux ssh config

--- main.tf
+++ main.tf
@@ -78,11 +78,22 @@ resource "aws_instance" "dev_node" {
   vpc_security_group_ids = [aws_security_group.mtv_sg.id]
   subnet_id              = aws_subnet.mtv_public_subnet.id
   user_data = file("userdata.tpl")
+  
   root_block_device {
     volume_size = 10
   }
 
   tags = {
     Name = "dev-node"
+  }
+
+  // Provisioners are a last resort. Tools like Ansible are better suited for configurations. Using provisioners this time as we are handling 1 server and for learning purposes.
+  provisioner "local-exec" {
+    command = templatefile("linux-ssh-config.tpl", {
+      hostname = self.map_public_ip
+      user = "ubuntu"
+      identityfile = "~./ssh/mtvkey"
+    })
+    interpreter = ["bash", "-c"]
   }
 }
